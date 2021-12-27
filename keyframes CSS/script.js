d61b5f74da5e09a67a4cccf595c1afeb1fc274be
Sxyntheon <jakob_kannenberg@icloud.com>
2021-12-27 14:38:13 +0100

    added favicon and navbar with clock. Also added responsive scaling

--- keyframes CSS/script.js
+++ keyframes CSS/script.js
@@ -1,3 +1,23 @@
 function slide() {
     document.getElementById("FillIn").classList.add("slide")    
 }
+
+function showTime() {
+    let date = new Date;
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    
+    hour = (hour < 10) ? "0" + hour : hour
+    minute = (minute < 10) ? "0" + minute : minute
+    second = (second < 10) ? "0" + second : second
+
+    let time = hour + ":" + minute + ":" + second;
+
+    document.getElementById("clock").innerHTML = time;
+    let timer = setTimeout(function(){showTime() }, 1000);
+
+}
+
+showTime()
